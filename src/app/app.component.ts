4947698fb3452f41a1908cdb1e0fdfbbdf59064c
Ganga <kpgangajayakodi@gmail.com>
2021-05-17 09:27:11 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {TaskService} from './service/task.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  constructor(public taskService: TaskService) {
+  }
+}
